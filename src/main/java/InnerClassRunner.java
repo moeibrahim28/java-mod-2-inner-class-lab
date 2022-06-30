3e60521d082e70c4f16472cb3b222b712421c98f
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-06-30 14:18:41 -0400

    changed method of choosing grading method

--- src/main/java/InnerClassRunner.java
+++ src/main/java/InnerClassRunner.java
@@ -1,10 +1,13 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class InnerClassRunner {
 
     public static void main(String[] args) {
-        String gradingMethod = "HEAVY"; // <---- simply change this value to change the way grades are curved
+        Scanner scanner = new Scanner(System.in);
+        String gradingMethod = getGradingMethod(scanner); // <---- simply change this value to change the way grades are
+                                                          // curved
         StudentGradeTranslator gradeTranslator = new StudentGradeTranslator(gradingMethod);
 
         HashMap<String, String> studentGrades = new HashMap<String, String>();
@@ -22,6 +25,41 @@ public class InnerClassRunner {
             System.out.println("Points till next letter grade "
                     + gradeTranslator.howManyForNextLetterGrade(Integer.parseInt(studentGrade.getValue())));
 
+        }
+        scanner.close();
+    }
+
+    public static String getGradingMethod(Scanner scanner) {
+        String gradingMethod = "";
+        System.out.println("Which method would you like to use for grading?\n1. Flat\n2. Slight\n3. Heavy");
+        String sampleString = scanner.nextLine();
+        int input = getInput(sampleString);
+        switch (input) {
+            case 1:
+                gradingMethod = "FLAT";
+                break;
+            case 2:
+                gradingMethod = "SLIGHT";
+                break;
+            case 3:
+                gradingMethod = "HEAVY";
+                break;
+            default:
+                System.out.println("You are bad at typing numbers 1 to 3");
+                gradingMethod = getGradingMethod(scanner);
+                break;
+        }
+
+        return gradingMethod;
+
+    }
+
+    private static int getInput(String sampleString) {
+        try {
+            return Integer.parseInt(sampleString);
+        } catch (Exception e) {
+
+            return -1;
         }
     }
 }
